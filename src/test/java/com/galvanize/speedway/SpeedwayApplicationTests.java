020d3298c5ae8ddaab01455e125c1c398ab5e9e0
bharathi1988 <bharathi1988@gmail.com>
2021-02-08 18:16:33 -0600

    added one dummy test to check the workflow

--- src/test/java/com/galvanize/speedway/SpeedwayApplicationTests.java
+++ src/test/java/com/galvanize/speedway/SpeedwayApplicationTests.java
@@ -3,11 +3,18 @@ package com.galvanize.speedway;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 @SpringBootTest
 class SpeedwayApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void dummyTest(){
+		assertTrue(true);
 	}
 
 }
